fd2b9397a061f6e73c0b7579b7db647de10095ea
Montelabella <hugachia@me.com>
2018-05-12 11:13:13 -0500

    first commit for seeds

--- MySQL Seeds/seeds.sql
+++ MySQL Seeds/seeds.sql
@@ -1,6 +1,40 @@
-INSERT INTO burgers
-    (burger_name)
-VALUES
-    ("Turkey burger with avocado"),
-    ("Classic cheeseburger"),
-    ("Mushroom & Swiss burger");
+INSERT INTO volunteers (first_name, last_name, phone_num, email, password)
+VALUES ("Tom", "Franklin", "913-555-2345", "tfranklin@hey.com", "h3llofr@nk");
+
+INSERT INTO volunteers (first_name, last_name, phone_num, email, password)
+VALUES ("Kalie", "Smith", "816-555-5432", "ksmith@hey.com", "1mK@tieS00");
+
+INSERT INTO volunteers , (first_name, last_name, phone_num, email, password)
+VALUES ("Pat", "Simms", "913-555-2233", "psimms@hey.com", "p@t1sHere12");
+
+INSERT INTO volunteers , (first_name, last_name, phone_num, email, password)
+VALUES ("James", "White", "816-555-9876", "jwhite@hey.com", "H3lp3rJ@mes");
+
+INSERT INTO volunteers , (first_name, last_name, phone_num, email, password)
+VALUES ("Sam", "Resende", "913-555-7643", "sresende@hey.com", "S@miAm01");
+
+INSERT INTO volunteers , (first_name, last_name, phone_num, email, password)
+VALUES ("Sonia", "Johnson", "913-555-1198", "sjohnson@hey.com", "J0hn$0n#!");
+
+INSERT INTO organization (organizer, email, phone_num, password)
+VALUES ("South Baptist Church", "sbc@church.org", "816-326-7643", "SBC!@#123");
+
+INSERT INTO organization (organizer, email, phone_num, password)
+VALUES ("Westend Ministry", "west@church.org", "816-235-1845", "Min1sry*$");
+
+INSERT INTO organization (organizer, email, phone_num, password)
+VALUES ("North High School", "nhs@dist.edu", "913-445-8365", "NHS@#$dist");
+
+INSERT INTO organization (organizer, email, phone_num, password)
+VALUES ("Blue Cross", "bchelpers@help.org", "913-234-9264", "blu3Cr0$$001");
+
+INSERT INTO events (event_name, event_desc, event_loc, event_date, event_time)
+VALUES ("Gardening", "Need help picking weeds out of our community garden.  Please
+understand it will be hot outside and to bring sunscreen.  Thank you for your help", "South Babtist Church", 12:00:00);
+
+INSERT INTO events (event_name, event_desc, event_loc, event_date, event_time)
+VALUES ("Car Wash", "Need some volunteers for our fundraiser car wash.  Please
+understand it will be hot outside and to bring sunscreen.  Thank you for your help", "North High School", 12:00:00);
+
+INSERT INTO events (event_name, event_desc, event_loc, event_date, event_time)
+VALUES ("Filing", "We are remodeling and would love some volunteers to help us organize and file!  Thank you for your help!", "Blue Cross", 12:00:00);
