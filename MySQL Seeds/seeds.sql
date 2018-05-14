98d7316ea39a33af4ae95d00e87b8fa14c24a450
Steven Bender <sbender1990@gmail.com>
2018-05-14 17:04:45 -0500

    sql stuff

--- MySQL Seeds/seeds.sql
+++ MySQL Seeds/seeds.sql
@@ -1,83 +1,41 @@
-<<<<<<< HEAD
-INSERT INTO volunteers (first_name, last_name, phone_num, email)
-VALUES ("Tom", "Franklin", "913-555-2345", "tfranklin@hey.com");
+
 
 INSERT INTO volunteers (first_name, last_name, phone_num, email)
-VALUES ("Kalie", "Smith", "816-555-5432", "ksmith@hey.com");
+VALUES 
+("Tom", "Franklin", "913-555-2345", "tfranklin@hey.com"),
+("Kalie", "Smith", "816-555-5432", "ksmith@hey.com"),
+("Pat", "Simms", "913-555-2233", "psimms@hey.com"),
+("James", "White", "816-555-9876", "jwhite@hey.com"),
+("Sam", "Resende", "913-555-7643", "sresende@hey.com"),
+("Sonia", "Johnson", "913-555-1198", "sjohnson@hey.com");
 
-INSERT INTO volunteers (first_name, last_name, phone_num, email)
-VALUES ("Pat", "Simms", "913-555-2233", "psimms@hey.com");
+INSERT INTO organizations (organizer, email, phone_num)
+VALUES 
+("South Baptist Church", "sbc@church.org", "816-326-7643"),
+("Westend Ministry", "west@church.org", "816-235-1845"),
+("North High School", "nhs@dist.edu", "913-445-8365"),
+("Blue Cross", "bchelpers@help.org", "913-234-9264");
 
-INSERT INTO volunteers (first_name, last_name, phone_num, email)
-VALUES ("James", "White", "816-555-9876", "jwhite@hey.com");
-
-INSERT INTO volunteers (first_name, last_name, phone_num, email)
-VALUES ("Sam", "Resende", "913-555-7643", "sresende@hey.com");
-
-INSERT INTO volunteers (first_name, last_name, phone_num, email)
-VALUES ("Sonia", "Johnson", "913-555-1198", "sjohnson@hey.com");
-
-INSERT INTO organization (organizer, email, phone_num)
-VALUES ("South Baptist Church", "sbc@church.org", "816-326-7643");
-
-INSERT INTO organization (organizer, email, phone_num)
-VALUES ("Westend Ministry", "west@church.org", "816-235-1845");
-
-INSERT INTO organization (organizer, email, phone_num)
-VALUES ("North High School", "nhs@dist.edu", "913-445-8365");
-
-INSERT INTO organization (organizer, email, phone_num)
-VALUES ("Blue Cross", "bchelpers@help.org", "913-234-9264");
+INSERT INTO users (username, password, hh_role)
+Values 
+("tfranklin@hey.com", "h3llofr@nk", "volunteer"), 
+("ksmith@hey.com", "1mK@tieS00", "volunteer"), 
+("psimms@hey.com", "p@t1sHere12", "volunteer"), 
+("jwhite@hey.com", "H3lp3rJ@mes", "volunteer"), 
+("sresende@hey.com", "S@miAm01", "volunteer"), 
+("sjohnson@hey.com", "J0hn$0n#!", "volunteer"), 
+("sbc@church.org", "SBC!@#123", "organization"), 
+("west@church.org", "Min1sry*$", "organization"), 
+("nhs@dist.edu", "NHS@#$dist", "organization"), 
+("bchelpers@help.org", "blu3Cr0$$001", "organization");
 
 INSERT INTO events (event_name, event_desc, event_loc, event_date, event_time)
 VALUES ("Gardening", "Need help picking weeds out of our community garden.  Please
-understand it will be hot outside and to bring sunscreen.  Thank you for your help!", "South Babtist Church", 12:00:00);
+understand it will be hot outside and to bring sunscreen.  Thank you for your help", "South Babtist Church", "2018-05-31", "12:00");
 
 INSERT INTO events (event_name, event_desc, event_loc, event_date, event_time)
 VALUES ("Car Wash", "Need some volunteers for our fundraiser car wash.  Please
-understand it will be hot outside and to bring sunscreen.  Thank you for your help!", "North High School", 12:00:00);
+understand it will be hot outside and to bring sunscreen.  Thank you for your help", "North High School", "2018-05-27", "12:00");
 
 INSERT INTO events (event_name, event_desc, event_loc, event_date, event_time)
-VALUES ("Filing", "We are remodeling and would love some volunteers to help us organize and file!  Thank you for your help!", "Blue Cross", 12:00:00);
-=======
-INSERT INTO volunteers (first_name, last_name, phone_num, email, password)
-VALUES ("Tom", "Franklin", "913-555-2345", "tfranklin@hey.com", "h3llofr@nk");
-
-INSERT INTO volunteers (first_name, last_name, phone_num, email, password)
-VALUES ("Kalie", "Smith", "816-555-5432", "ksmith@hey.com", "1mK@tieS00");
-
-INSERT INTO volunteers , (first_name, last_name, phone_num, email, password)
-VALUES ("Pat", "Simms", "913-555-2233", "psimms@hey.com", "p@t1sHere12");
-
-INSERT INTO volunteers , (first_name, last_name, phone_num, email, password)
-VALUES ("James", "White", "816-555-9876", "jwhite@hey.com", "H3lp3rJ@mes");
-
-INSERT INTO volunteers , (first_name, last_name, phone_num, email, password)
-VALUES ("Sam", "Resende", "913-555-7643", "sresende@hey.com", "S@miAm01");
-
-INSERT INTO volunteers , (first_name, last_name, phone_num, email, password)
-VALUES ("Sonia", "Johnson", "913-555-1198", "sjohnson@hey.com", "J0hn$0n#!");
-
-INSERT INTO organization (organizer, email, phone_num, password)
-VALUES ("South Baptist Church", "sbc@church.org", "816-326-7643", "SBC!@#123");
-
-INSERT INTO organization (organizer, email, phone_num, password)
-VALUES ("Westend Ministry", "west@church.org", "816-235-1845", "Min1sry*$");
-
-INSERT INTO organization (organizer, email, phone_num, password)
-VALUES ("North High School", "nhs@dist.edu", "913-445-8365", "NHS@#$dist");
-
-INSERT INTO organization (organizer, email, phone_num, password)
-VALUES ("Blue Cross", "bchelpers@help.org", "913-234-9264", "blu3Cr0$$001");
-
-INSERT INTO events (event_name, event_desc, event_loc, event_date, event_time)
-VALUES ("Gardening", "Need help picking weeds out of our community garden.  Please
-understand it will be hot outside and to bring sunscreen.  Thank you for your help", "South Babtist Church", 12:00:00);
-
-INSERT INTO events (event_name, event_desc, event_loc, event_date, event_time)
-VALUES ("Car Wash", "Need some volunteers for our fundraiser car wash.  Please
-understand it will be hot outside and to bring sunscreen.  Thank you for your help", "North High School", 12:00:00);
-
-INSERT INTO events (event_name, event_desc, event_loc, event_date, event_time)
-VALUES ("Filing", "We are remodeling and would love some volunteers to help us organize and file!  Thank you for your help!", "Blue Cross", 12:00:00);
->>>>>>> master
+VALUES ("Filing", "We are remodeling and would love some volunteers to help us organize and file!  Thank you for your help!", "Blue Cross", "2018-05-17", "12:00");
--- app/config/connection.js
+++ app/config/connection.js
@@ -6,7 +6,7 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("helping_hands", "root", "", {
+var sequelize = new Sequelize("helping_hands", "root", "50miler", {
   host: "localhost",
   dialect: "mysql",
   pool: {
